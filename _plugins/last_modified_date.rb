1292de97ddac4b5297516aac44a6f708f40350bd
GamingWithEvets <thenewgwe@gmail.com>
2023-04-23 11:13:41 +0700

    Site update

--- _plugins/last_modified_date.rb
+++ _plugins/last_modified_date.rb
@@ -5,7 +5,7 @@ module Jekyll
   class LastModifiedDateTag < Liquid::Tag
     def render(context)
       page = context.registers[:page]
-      format_string = context.registers[:site].config['last_edit_time_format']
+      format_string = context.registers[:site].config['last_edit_time_format']	
 
       if ENV['GITHUB_REPOSITORY']
         page['last_modified_date'] = get_last_commit_date(page['path'])
@@ -17,17 +17,29 @@ module Jekyll
     end
 
     def get_last_commit_date(path)
-      repo_name = ENV['GITHUB_REPOSITORY']
-      repo_owner = repo_name.split('/').first
-      repo_name = repo_name.split('/').last
+      gh_repo = ENV['GITHUB_REPOSITORY']
+      repo_owner = gh_repo.split('/').first
+      repo_name = gh_repo.split('/').last
 
       url = "https://api.github.com/repos/#{repo_owner}/#{repo_name}/commits?path=#{path}&per_page=1"
       uri = URI(url)
 
-      response = Net::HTTP.get(uri)
+	  response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == 'https') do |http|
+		request = Net::HTTP::Get.new(uri)
+		request['authorization'] = "Bearer #{ENV['token']}"
+		http.request(request)
+	  end
+	  
       json = JSON.parse(response)
+	  
+	  begin
+	    if json['message'].start_with?("API rate limit exceeded for")
+		  return Time.now
+		end
+	  rescue StandardError
+	  end
 
-      if json.empty?
+      if json.nil? || json.empty?
         return Time.now
       end
 
